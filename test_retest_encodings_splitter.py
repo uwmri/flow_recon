30a8e15fe9851015e66363f1e3276be169248c0f
Ben Awad <bawad2@wisc.edu>
2025-11-04 13:48:13 -0600

    used numba to make faster

--- test_retest_encodings_splitter.py
+++ test_retest_encodings_splitter.py
@@ -1,37 +1,60 @@
 #%%
 import h5py
 import numpy as np
+#%%
+with h5py.File('/mounts/data/analyses/bawad2/testData/Images.h5', 'r') as hf:
+    temp_img = hf['IMAGE'][:]
+    temp_mag = hf['IMAGE_MAG'][:]
+    temp_phase = hf['IMAGE_PHASE'][:]
+print(f"Loaded image shape: {temp_img.shape}")
+print(f"Loaded mag shape: {temp_mag.shape}")
+print(f"Loaded phase shape: {temp_phase.shape}")
+#%%
+enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8 = [temp_img[:, i, :, :, :] for i in range(8)]
+encs = np.stack([enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8], axis=1)
 
-#%% Print out shape of .h5 file
-with h5py.File('/mounts/data/analyses/bawad2/testData/Images.h5', 'r') as f:
+img1 = np.concatenate((encs[:, [0]], encs[:,[5]], encs[:, [2]], encs[:,[7]]), axis=1)
+img2 = np.concatenate((encs[:, [4]], encs[:,[1]], encs[:, [6]], encs[:,[3]]), axis=1)
+
+print(f"Image1 shape: {img1.shape}")
+print(f"Image2 shape: {img2.shape}")
+
+enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8 = [temp_mag[:, i, :, :, :] for i in range(8)]
+encs = np.stack([enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8], axis=1)
+
+mag1 = np.concatenate((encs[:, [0]], encs[:,[5]], encs[:, [2]], encs[:,[7]]), axis=1)
+mag2 = np.concatenate((encs[:, [4]], encs[:,[1]], encs[:, [6]], encs[:,[3]]), axis=1)
+
+print(f"Magnitude1 shape: {mag1.shape}")
+print(f"Magnitude2 shape: {mag2.shape}")
+
+enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8 = [temp_phase[:, i, :, :, :] for i in range(8)]
+encs = np.stack([enc1, enc2, enc3, enc4, enc5, enc6, enc7, enc8], axis=1)
+
+phase1 = np.concatenate((encs[:, [0]], encs[:,[5]], encs[:, [2]], encs[:,[7]]), axis=1)
+phase2 = np.concatenate((encs[:, [4]], encs[:,[1]], encs[:, [6]], encs[:,[3]]), axis=1)
+
+print(f"Phase1 shape: {phase1.shape}")
+print(f"Phase2 shape: {phase2.shape}")
+#%%
+with h5py.File('Images1.h5', 'w') as hf:  
+    hf.create_dataset('IMAGE', data=img1)
+    hf.create_dataset('IMAGE_MAG', data=mag1)
+    hf.create_dataset('IMAGE_PHASE', data=phase1)
+    print("Datasets:", list(hf.keys()))
+
+with h5py.File('Images2.h5', 'w') as hf:  
+    hf.create_dataset('IMAGE', data=img2)
+    hf.create_dataset('IMAGE_MAG', data=mag2)
+    hf.create_dataset('IMAGE_PHASE', data=phase2)
+    print("Datasets:", list(hf.keys()))
+#%%
+with h5py.File('Images1.h5', 'r') as hf:
+    print("Datasets:", list(hf.keys()))
     ds_tot = ["IMAGE", "IMAGE_MAG", "IMAGE_PHASE"]
     img = [[] for _ in range(8)]
     for name in ds_tot:
-        if name in f:
-            ds = f[name]
+        if name in hf:
+            ds = hf[name]
             print(f"Dimensions of dataset: '{name}': {ds.shape}")
         img[0]
-
-        #for i in range(8):
-        #    img[i].append(ds0[:[i]:::])
-#%% Splitting the files into their own components
-with h5py.File('/mounts/data/analyses/bawad2/testData/Images.h5', 'r') as hf:
-    temp_img = hf['IMAGE']
-    # temp_mag = hf['IMAGE_MAG']
-    # temp_phase = hf['IMAGE_PHASE']
-    
-    encs=[]
-    for i in range(temp_img.shape[1]):
-        encs = np.array(temp_img[:, i:i+1, :, :, :])
-        print(f"encs[{i}] shape: {temp_img[:, i:i+1, :, :, :].shape}")
-#%%
-enc1 = encs[0]; enc6 = encs[5]; enc3 = encs[2]; enc8 = encs[7]
-enc5 = encs[4]; enc2 = encs[1]; enc7 = encs[6]; enc4 = encs[3]
-img1 = np.concatenate((enc1, enc6, enc3, enc8), axis=1)
-img2 = np.concatenate((enc5, enc2, enc7, enc4), axis=1)
-print(np.array(img1).shape)
-print(np.array(img2).shape)
-#%%
-with h5py.File('/mounts/data/analyses/bawad2/testData/Images1.h5', 'w'):  
-    hf.create_dataset("IMAGE", data=img1)
-#%%
