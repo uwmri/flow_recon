a2860e0cb586ca497d09ba1c21a472d77eff2e91
Benjamin Awad <bawad2@wisc.edu>
2026-09-14 13:17:24 -0500

    made split_type auto update with filename

--- test_retest_encodings_splitter.py
+++ test_retest_encodings_splitter.py
@@ -1,166 +1,162 @@
-#%%
-import re
-import h5py
 from pathlib import Path
-
-from sympy import O
-
-# Split from MRI_Raw.h5
-# def mri_raw_splitter(inputFile, outputImg1, outputImg2, encodeOrder = "interleaf"):
-
-inputFile = Path("/home/bxa033/Data/CVMRIGroup/Users/bxa033/trtstudyvol2/espirit/pils/pythonRecon/8enc/MRI_Raw.h5")
-encodeOrder = "interleaf"
-outputImg1 = "/home/bxa033/Data/CVMRIGroup/Users/bxa033/trtstudyvol2/espirit/pils/pythonRecon/8enc/MRI_Raw1.h5"
-outputImg2 = "/home/bxa033/Data/CVMRIGroup/Users/bxa033/trtstudyvol2/espirit/pils/pythonRecon/8enc/MRI_Raw2.h5"
-import h5py
-import re
-
-if encodeOrder == "interleaf":
-
-    output_sets = {
-        outputImg1: [0, 5, 2, 7],
-        outputImg2: [4, 1, 6, 3]
-    }
-
-elif encodeOrder == "regular":
-
-    output_sets = {
-        outputImg1: [0, 1, 2, 3],
-        outputImg2: [4, 5, 6, 7]
-    }
-
-else:
-    raise ValueError(
-        f"Unknown encodeOrder: {encodeOrder}. "
-        "Use 'interleaf' or 'regular'."
-    )
-
-
-groups_to_split = ["Gating", "Kdata"]
-
-
-with h5py.File(inputFile, "r") as hf:
-
-    for output_name, encodes in output_sets.items():
-
-        encode_map = {
-            old_encode: new_encode
-            for new_encode, old_encode in enumerate(encodes)
-        }
-        
-        print(f"Encoding map: {encode_map}")
-
-        print(f"\nCreating: {output_name}")
-        print(f"Encodings: {encodes}")
-
-        with h5py.File(output_name, "w") as out_hf:
-
-            for group_name in groups_to_split:
-
-                if group_name not in hf:
-                    print(f"/{group_name} not found")
-                    continue
-
-                input_group = hf[group_name]
-                output_group = out_hf.create_group(group_name)
-
-                for key in input_group.keys():
-
-                    # Find _E# anywhere in the name
-                    match = re.search(r"_E(\d+)(?:_|$)", key)
-
-                    if match is None:
-                        print(f"SKIPPING: /{group_name}/{key} -- no encoding found")
-                        continue
-
-                    encode_num = int(match.group(1))
-                    
-                    if encode_num in encode_map:
-                        new_encode = encode_map[encode_num]
-                        
-                        new_key = re.sub(r"_E\d+",
-                                         f"_E{new_encode}",
-                                         key,
-                                         count=1
-                                         )
-                        
-                        print(
-                            f"/{group_name}/{key} -> /{group_name}/{new_key}"
-                        )
-
-                        input_group.copy(
-                            key,
-                            output_group,
-                            name=new_key
-                        )    
-    
-#%% Split from Images.h5
 import argparse
 import h5py
 import numpy as np
 
+def encSplitter(split_type, inputFile, outputImg1, outputImg2, encodeOrder = "interleaf"):
 
-def images_encoding_splitter(inputFile, outputImg1, outputImg2, encodeOrder="interleaf"):
+    # Split from MRI_Raw.h5
+    def mri_raw_enc_splitter(inputFile, outputImg1, outputImg2, encodeOrder = "interleaf"):
 
-    with h5py.File(inputFile, 'r') as hf:
-        temp_img = hf['IMAGE'][:]
-        temp_mag = hf['IMAGE_MAG'][:]
-        temp_phase = hf['IMAGE_PHASE'][:]
+        if encodeOrder == "interleaf":
 
-    print(f"Loaded image shape: {temp_img.shape}")
-    print(f"Loaded mag shape: {temp_mag.shape}")
-    print(f"Loaded phase shape: {temp_phase.shape}")
+            output_sets = {
+                outputImg1: [0, 5, 2, 7],
+                outputImg2: [4, 1, 6, 3]
+            }
 
-    if encodeOrder == "interleaf":
-        order1 = [0, 5, 2, 7]
-        order2 = [4, 1, 6, 3]
+        elif encodeOrder == "regular":
 
-    elif encodeOrder == "regular":
-        order1 = [0, 1, 2, 3]
-        order2 = [4, 5, 6, 7]
+            output_sets = {
+                outputImg1: [0, 1, 2, 3],
+                outputImg2: [4, 5, 6, 7]
+            }
 
+        else:
+            raise ValueError(
+                f"Unknown encodeOrder: {encodeOrder}. "
+                "Use 'interleaf' or 'regular'."
+            )
+
+
+        groups_to_split = ["Gating", "Kdata"]
+
+
+        with h5py.File(inputFile, "r") as hf:
+
+            for output_name, encodes in output_sets.items():
+
+                encode_map = {
+                    old_encode: new_encode
+                    for new_encode, old_encode in enumerate(encodes)
+                }
+                
+                print(f"Encoding map: {encode_map}")
+
+                print(f"\nCreating: {output_name}")
+                print(f"Encodings: {encodes}")
+
+                with h5py.File(output_name, "w") as out_hf:
+
+                    for group_name in groups_to_split:
+
+                        if group_name not in hf:
+                            print(f"/{group_name} not found")
+                            continue
+
+                        input_group = hf[group_name]
+                        output_group = out_hf.create_group(group_name)
+
+                        for key in input_group.keys():
+
+                            # Find _E# anywhere in the name
+                            match = re.search(r"_E(\d+)(?:_|$)", key)
+
+                            if match is None:
+                                print(f"SKIPPING: /{group_name}/{key} -- no encoding found")
+                                continue
+
+                            encode_num = int(match.group(1))
+                            
+                            if encode_num in encode_map:
+                                new_encode = encode_map[encode_num]
+                                
+                                new_key = re.sub(r"_E\d+",
+                                                f"_E{new_encode}",
+                                                key,
+                                                count=1
+                                                )
+                                
+                                print(
+                                    f"/{group_name}/{key} -> /{group_name}/{new_key}"
+                                )
+
+                                input_group.copy(
+                                    key,
+                                    output_group,
+                                    name=new_key
+                                )    
+
+    # Split from Images.h5
+    def images_encoding_splitter(inputFile, outputImg1, outputImg2, encodeOrder="interleaf"):
+
+        with h5py.File(inputFile, 'r') as hf:
+            temp_img = hf['IMAGE'][:]
+            temp_mag = hf['IMAGE_MAG'][:]
+            temp_phase = hf['IMAGE_PHASE'][:]
+
+        print(f"Loaded image shape: {temp_img.shape}")
+        print(f"Loaded mag shape: {temp_mag.shape}")
+        print(f"Loaded phase shape: {temp_phase.shape}")
+
+        if encodeOrder == "interleaf":
+            order1 = [0, 5, 2, 7]
+            order2 = [4, 1, 6, 3]
+
+        elif encodeOrder == "regular":
+            order1 = [0, 1, 2, 3]
+            order2 = [4, 5, 6, 7]
+
+        else:
+            raise ValueError(
+                f"Unknown encodeOrder: {encodeOrder}. "
+                "Use 'interleaf' or 'regular'."
+            )
+
+        # Split/reorder encodings
+        img1 = temp_img[:, order1, ...]
+        img2 = temp_img[:, order2, ...]
+
+        mag1 = temp_mag[:, order1, ...]
+        mag2 = temp_mag[:, order2, ...]
+
+        phase1 = temp_phase[:, order1, ...]
+        phase2 = temp_phase[:, order2, ...]
+
+        print(f"Image1 shape: {img1.shape}")
+        print(f"Image2 shape: {img2.shape}")
+        print(f"Magnitude1 shape: {mag1.shape}")
+        print(f"Magnitude2 shape: {mag2.shape}")
+        print(f"Phase1 shape: {phase1.shape}")
+        print(f"Phase2 shape: {phase2.shape}")
+
+        # Write Image 1
+        with h5py.File(outputImg1, 'w') as hf:
+            hf.create_dataset('IMAGE', data=img1)
+            hf.create_dataset('IMAGE_MAG', data=mag1)
+            hf.create_dataset('IMAGE_PHASE', data=phase1)
+
+            print(f"Created: {outputImg1}")
+            print("Datasets:", list(hf.keys()))
+
+        # Write Image 2
+        with h5py.File(outputImg2, 'w') as hf:
+            hf.create_dataset('IMAGE', data=img2)
+            hf.create_dataset('IMAGE_MAG', data=mag2)
+            hf.create_dataset('IMAGE_PHASE', data=phase2)
+
+            print(f"Created: {outputImg2}")
+            print("Datasets:", list(hf.keys()))
+
+    input_lower = str(inputFile).lower()
+
+    if "mri_raw" in input_lower:
+        mri_raw_enc_splitter(inputFile, outputImg1, outputImg2, encodeOrder = "interleaf")
+    elif "images" in input_lower:
+        images_encoding_splitter(inputFile, outputImg1, outputImg2, encodeOrder = "interleaf")
     else:
-        raise ValueError(
-            f"Unknown encodeOrder: {encodeOrder}. "
-            "Use 'interleaf' or 'regular'."
-        )
+        raise ValueError(f"Could not determine input file type from: {inputFile}. Expected filename to contain either 'mri_raw' or 'images' ")
 
-    # Split/reorder encodings
-    img1 = temp_img[:, order1, ...]
-    img2 = temp_img[:, order2, ...]
-
-    mag1 = temp_mag[:, order1, ...]
-    mag2 = temp_mag[:, order2, ...]
-
-    phase1 = temp_phase[:, order1, ...]
-    phase2 = temp_phase[:, order2, ...]
-
-    print(f"Image1 shape: {img1.shape}")
-    print(f"Image2 shape: {img2.shape}")
-    print(f"Magnitude1 shape: {mag1.shape}")
-    print(f"Magnitude2 shape: {mag2.shape}")
-    print(f"Phase1 shape: {phase1.shape}")
-    print(f"Phase2 shape: {phase2.shape}")
-
-    # Write Image 1
-    with h5py.File(outputImg1, 'w') as hf:
-        hf.create_dataset('IMAGE', data=img1)
-        hf.create_dataset('IMAGE_MAG', data=mag1)
-        hf.create_dataset('IMAGE_PHASE', data=phase1)
-
-        print(f"Created: {outputImg1}")
-        print("Datasets:", list(hf.keys()))
-
-    # Write Image 2
-    with h5py.File(outputImg2, 'w') as hf:
-        hf.create_dataset('IMAGE', data=img2)
-        hf.create_dataset('IMAGE_MAG', data=mag2)
-        hf.create_dataset('IMAGE_PHASE', data=phase2)
-
-        print(f"Created: {outputImg2}")
-        print("Datasets:", list(hf.keys()))
-
-#%%
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Split an 8-encoding HDF5 image into two 4-encoding images.")
@@ -175,7 +171,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    images_encoding_splitter(
+    encSplitter(
         args.input,
         args.output1,
         args.output2,
